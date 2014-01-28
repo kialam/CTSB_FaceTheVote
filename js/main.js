ca6053210ec8adfc9f00fe6d793bd4380ec10454
Kia Lam <digitalanime@gmail.com>
2014-01-27 18:32:13 -0800

    added new thumbsnails

--- js/main.js
+++ js/main.js
@@ -13,13 +13,7 @@
         overlayCC = overlay.getContext('2d');
         smileTracker = new SmileTracker();
         
-        // ranking
-<<<<<<< HEAD
-        // ranking = new Ranking();
-=======
-//        ranking = new Ranking();
->>>>>>> 657d46a3dd3c4e918e4b8f159c03c7746d39ef6d
-        
+
         // pages
         intro = $('.intro');
         permission = $('.permission');
